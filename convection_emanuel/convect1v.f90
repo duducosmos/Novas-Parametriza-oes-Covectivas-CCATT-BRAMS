fc30d63e9254d6c975a164c2d664f1187093e0da
Eduardo dos Santos Pereira <pereira.somoza@gmail.com>
2011-04-20 15:22:53 -0300

    Criação da subrotina de teste e inicialização -copem_set_matriz_at1, criação da subrotina de desalocagem de vetores e matrizes copem_free_matriz,  mudança do nome do modulo pra copem - convective parametrization of emanuel scheme, adoção do padrão para escrever subrotinas como copem_* - nome do modulo seguido do nome da subrotina

--- convection_emanuel/convect1v.f90
+++ convection_emanuel/convect1v.f90
@@ -1,71 +1,6 @@
                   
-module convect_emanuel
-    implicit none
-
-! -----------------------------------------------------------------------
-!
-!   ***                     Specify Switches                         ***
-!
-!   ***   IPBL: Set to zero to bypass dry adiabatic adjustment       ***
-!   ***    Any other value results in dry adiabatic adjustment       ***
-!   ***     (Zero value recommended for use in models with           ***
-!   ***                   boundary layer schemes)                    ***
-!
-!   ***   MINORIG: Lowest level from which convection may originate  ***
-!   ***     (Should be first model level at which T is defined       ***
-!   ***      for models using bulk PBL schemes; otherwise, it should ***
-!   ***      be the first model level at which T is defined above    ***
-!   ***                      the surface layer)                      ***
-
-    integer, parameter :: IPBL=0,MINORIG=1
-!
-!------------------------------------------------------------------------------
-!
-!   ***                    SPECIFY PARAMETERS                        ***
-!
-!   *** ELCRIT IS THE AUTOCONVERSION THERSHOLD WATER CONTENT (gm/gm) ***
-!   ***  TLCRIT IS CRITICAL TEMPERATURE BELOW WHICH THE AUTO-        ***
-!   ***       CONVERSION THRESHOLD IS ASSUMED TO BE ZERO             ***
-!   ***     (THE AUTOCONVERSION THRESHOLD VARIES LINEARLY            ***
-!   ***               BETWEEN 0 C AND TLCRIT)                        ***
-!   ***   ENTP IS THE COEFFICIENT OF MIXING IN THE ENTRAINMENT       ***
-!   ***                       FORMULATION                            ***
-!   ***  SIGD IS THE FRACTIONAL AREA COVERED BY UNSATURATED DNDRAFT  ***
-!   ***  SIGS IS THE FRACTION OF PRECIPITATION FALLING OUTSIDE       ***
-!   ***                        OF CLOUD                              ***
-!   ***        OMTRAIN IS THE ASSUMED FALL SPEED (P/s) OF RAIN       ***
-!   ***     OMTSNOW IS THE ASSUMED FALL SPEED (P/s) OF SNOW          ***
-!   ***  COEFFR IS A COEFFICIENT GOVERNING THE RATE OF EVAPORATION   ***
-!   ***                          OF RAIN                             ***
-!   ***  COEFFS IS A COEFFICIENT GOVERNING THE RATE OF EVAPORATION   ***
-!   ***                          OF SNOW                             ***
-!   ***     CU IS THE COEFFICIENT GOVERNING CONVECTIVE MOMENTUM      ***
-!   ***                         TRANSPORT                            ***
-!   ***    DTMAX IS THE MAXIMUM NEGATIVE TEMPERATURE PERTURBATION    ***
-!   ***        A LIFTED PARCEL IS ALLOWED TO HAVE BELOW ITS LFC      ***
-!   ***    ALPHA AND DAMP ARE PARAMETERS THAT CONTROL THE RATE OF    ***
-!   ***                 APPROACH TO QUASI-EQUILIBRIUM                ***
-!   ***   (THEIR STANDARD VALUES ARE  0.20 AND 0.1, RESPECTIVELY)    ***
-!   ***                   (DAMP MUST BE LESS THAN 1)                 ***
-!
-
-    real(kind=8), parameter :: ELCRIT=0.0011,TLCRIT=-55.0,ENTP=1.5,SIGD=0.05,SIGS=0.12, &
-                               OMTRAIN=50.0,OMTSNOW=5.5,COEFFR=1.0,COEFFS=0.8,CU=0.7,   &
-                               BETA=10.0,DTMAX=0.9,ALPHA=0.2,DAMP=0.1
-
-!   ***        ASSIGN VALUES OF THERMODYNAMIC CONSTANTS,        ***
-!   ***            GRAVITY, AND LIQUID WATER DENSITY.           ***
-!   ***             THESE SHOULD BE CONSISTENT WITH             ***
-!   ***              THOSE USED IN CALLING PROGRAM              ***
-!   ***     NOTE: THESE ARE ALSO SPECIFIED IN SUBROUTINE TLIFT  ***
-
-    real(kind=8), parameter :: CPD=1005.7,CPV=1870.0,CL=2500.0,RV=461.5,RD=287.04,LV0=2.501E6,&
-                               G=9.8,ROWL=1000.0 
-    real(kind=8), parameter :: CPVMCL=CL-CPV, EPS=RD/RV, EPSI=1.0/EPS, GINV=1.0/G 
-    private AdiabaticAdjustment
-
-    contains
-
+module copem !Convective parametrization based in K. A. Emanuel (1991,1999) scheme
+   implicit none 
 !-----------------------------------------------------------------------------
 !    *** On input:      ***
 !
@@ -120,6 +55,15 @@ module convect_emanuel
 !
 !     DELT: The model time step (sec) between calls to CONVECT
 !
+
+    real,allocatable ,dimension(:) ::  T,Q,QS,U,V,P,PH
+    real,allocatable, dimension(:,:) :: TRA
+    integer :: ND !It will be obtained by using size function.
+    integer :: NTRA !It will be obtained by using size function.
+    integer :: NL
+
+
+
 !----------------------------------------------------------------------------
 !    ***   On Output:         ***
 !
@@ -143,6 +87,9 @@ module convect_emanuel
 !                 4               Moist convection occurs, but a CFL condition
 !                                 on the subsidence warming is violated. This
 !                                 does not cause the scheme to terminate.
+
+    integer :: IFLAG
+
 !
 !    FT:   Array of temperature tendency (K/s) of dimension ND, defined at same
 !            grid levels as T, Q, QS and P.
@@ -154,9 +101,13 @@ module convect_emanuel
 !            defined at same grid levels as T.
 !
 !    FV:   Same as FU, but for forcing of meridional velocity.
+
+    real,allocatable, dimension(:) ::  FT,FQ,FU,FV !Dimension ND
+
 !
 !    FTRA: Array of forcing of tracer content, in tracer mixing ratio per
 !            second, defined at same levels as T. Dimensioned (ND,NTRA).
+
 !
 !     PRECIP: Scalar convective precipitation rate (mm/day).
 !
@@ -177,76 +128,161 @@ module convect_emanuel
 !              ITS NEXT CALL. That is, the value of CBMF must be "remembered"
 !              by the calling program between calls to CONVECT.
 
-
-        subroutine convection(T,   Q,    QS,     U,    V,      TRA,    P,    PH,&
-                              ND,  NL,   NTRA,   DELT, IFLAG,  FT,     FQ,   FU,&
-                              FV,  FTRA, PRECIP, WD,   TPRIME, QPRIME, CBMF)
-
-            
-
-            
-            
-
-            !    ***  THE PARAMETER NA SHOULD IN GENERAL BE GREATER THAN   ***
+!    ***  THE PARAMETER NA SHOULD IN GENERAL BE GREATER THAN   ***
             !    ***                OR EQUAL TO  ND + 1                    ***
 
-            integer, parameter :: NA=70
-            
-            !
-            real(kind=8), dimension(NA) :: NENT,UP,VP,M,MP,TVP,TV,WATER,QP,EP,TH,WT,EVAP,CLW, &
-                                      SIGP,TP,TOLD,CPN,LV,LVCP,H,HP,GZ,HM,TRATM
-            !
-            integer :: ND,NL,NTRA,IFLAG
-            integer :: i,j,k,l
-            real(kind=8), dimension(ND) ::  T,Q,QS,U,V,P,PH,FT,FQ,FU,FV
-            real(kind=8), dimension(ND,NTRA) :: TRA,FTRA
-            real(kind=8), dimension(NA,NA) :: UENT,VENT,MENT,QENT,ELIJ,SIJ
-            real(kind=8), dimension(NA,NTRA) :: TRAP
-            real(kind=8), dimension(NA,NA,NTRA) :: TRAENT
-            real(kind=8) ::DELT,PRECIP,WD,TPRIME,QPRIME,DELTI,RDCP,CBMF
-            
+    integer :: NA ! NA >= ND+1            
+    !
 
-            DELTI=1.0/DELT
+    real, allocatable,dimension(:,:) :: FTRA !ND, NTRA
+    real ::PRECIP,WD,TPRIME,QPRIME,CBMF
 
 
+    !Variables that are common in the subroutines
+    !DELT is the time steep that copem_convection is called by principal program
+    real DELT,DELTI,RDCP
+    real, allocatable,dimension(:,:) :: UENT,VENT,MENT,QENT,ELIJ,SIJ  !NA:NA
+    real, allocatable,dimension(:,:) :: TRAP  !Na:NTRA
+    real, allocatable,dimension(:,:,:) :: TRAENT !NA:NA:NTRA
+
+
+    real, allocatable,dimension(:) :: NENT,UP,VP,M,MP,TVP,TV,WATER,QP,EP,TH,WT,EVAP,CLW, &
+                                  SIGP,TP,TOLD,CPN,LV,LVCP,H,HP,GZ,HM  !All this matriz have dimension equal NA
+    !
+    integer :: i,j,k,l
+
+
+! -----------------------------------------------------------------------
+!
+!   ***                     Specify Switches                         ***
+!
+!   ***   IPBL: Set to zero to bypass dry adiabatic adjustment       ***
+!   ***    Any other value results in dry adiabatic adjustment       ***
+!   ***     (Zero value recommended for use in models with           ***
+!   ***                   boundary layer schemes)                    ***
+!
+!   ***   MINORIG: Lowest level from which convection may originate  ***
+!   ***     (Should be first model level at which T is defined       ***
+!   ***      for models using bulk PBL schemes; otherwise, it should ***
+!   ***      be the first model level at which T is defined above    ***
+!   ***                      the surface layer)                      ***
+
+    integer, parameter :: IPBL=0,MINORIG=1
+!
+!------------------------------------------------------------------------------
+!
+!   ***                    SPECIFY PARAMETERS                        ***
+!
+!   *** ELCRIT IS THE AUTOCONVERSION THERSHOLD WATER CONTENT (gm/gm) ***
+!   ***  TLCRIT IS CRITICAL TEMPERATURE BELOW WHICH THE AUTO-        ***
+!   ***       CONVERSION THRESHOLD IS ASSUMED TO BE ZERO             ***
+!   ***     (THE AUTOCONVERSION THRESHOLD VARIES LINEARLY            ***
+!   ***               BETWEEN 0 C AND TLCRIT)                        ***
+!   ***   ENTP IS THE COEFFICIENT OF MIXING IN THE ENTRAINMENT       ***
+!   ***                       FORMULATION                            ***
+!   ***  SIGD IS THE FRACTIONAL AREA COVERED BY UNSATURATED DNDRAFT  ***
+!   ***  SIGS IS THE FRACTION OF PRECIPITATION FALLING OUTSIDE       ***
+!   ***                        OF CLOUD                              ***
+!   ***        OMTRAIN IS THE ASSUMED FALL SPEED (P/s) OF RAIN       ***
+!   ***     OMTSNOW IS THE ASSUMED FALL SPEED (P/s) OF SNOW          ***
+!   ***  COEFFR IS A COEFFICIENT GOVERNING THE RATE OF EVAPORATION   ***
+!   ***                          OF RAIN                             ***
+!   ***  COEFFS IS A COEFFICIENT GOVERNING THE RATE OF EVAPORATION   ***
+!   ***                          OF SNOW                             ***
+!   ***     CU IS THE COEFFICIENT GOVERNING CONVECTIVE MOMENTUM      ***
+!   ***                         TRANSPORT                            ***
+!   ***    DTMAX IS THE MAXIMUM NEGATIVE TEMPERATURE PERTURBATION    ***
+!   ***        A LIFTED PARCEL IS ALLOWED TO HAVE BELOW ITS LFC      ***
+!   ***    ALPHA AND DAMP ARE PARAMETERS THAT CONTROL THE RATE OF    ***
+!   ***                 APPROACH TO QUASI-EQUILIBRIUM                ***
+!   ***   (THEIR STANDARD VALUES ARE  0.20 AND 0.1, RESPECTIVELY)    ***
+!   ***       real DELT,DELTI,RDCP,             (DAMP MUST BE LESS THAN 1)                 ***
+!
+
+    real, parameter :: ELCRIT=0.0011,TLCRIT=-55.0,ENTP=1.5,SIGD=0.05,SIGS=0.12, &
+                               OMTRAIN=50.0,OMTSNOW=5.5,COEFFR=1.0,COEFFS=0.8,CU=0.7,   &
+                               BETA=10.0,DTMAX=0.9,ALPHA=0.2,DAMP=0.1
+
+!   ***        ASSIGN VALUES OF THERMODYNAMIC CONSTANTS,        ***
+!   ***            GRAVITY, AND LIQUID WATER DENSITY.           ***
+!   ***             THESE SHOULD BE CONSISTENT WITH             ***
+!   ***              THOSE USED IN CALLING PROGRAM              ***
+!   ***     NOTE: THESE ARE ALSO SPECIFIED IN SUBROUTINE TLIFT  ***
+
+    real, parameter :: CPD=1005.7,CPV=1870.0,CL=2500.0,RV=461.5,RD=287.04,LV0=2.501E6,&
+                               G=9.8,ROWL=1000.0 
+    real, parameter :: CPVMCL=CL-CPV, EPS=RD/RV, EPSI=1.0/EPS, GINV=1.0/G
+ 
+    logical :: T1_EntryMatriz ! This Variable is used by copem_set_matriz_at1, if the input parameters are O.K. the code continue and this varyable is set as .TRUE. 
+    private T1_EntryMatriz
+    private copem_AdiabaticAdjustment,copem_set_matriz_at1,copem_free_matriz
+
+
+
+    contains
+
+
+
+        subroutine copem_convection(T1,   Q1,    QS1,     U1,    V1,      TRA1,    P1,    PH1,&
+                              NL, DELT, IFLAG,  FT,     FQ,   FU,&
+                              FV,  FTRA, PRECIP, WD,   TPRIME, QPRIME, CBMF)    
+            implicit none 
+            real, dimension(:) :: T1,Q1,QS1,U1,V1,P1,PH1
+            real, dimension(:,:) :: TRA1,FTRA
+   
+            real, dimension(:) :: FT,FQ,FU,FV
+            integer :: NL,IFLAG
+            real :: DELT,PRECIP,WD,TPRIME,QPRIME,CBMF
            
 
+            call copem_set_matriz_at1(T1,Q1,QS1,U1,V1,TRA1,P1,PH1) ! The first automatic test of this parametrization, difine if  T1_EntryMatriz will be .TRUE. or .FALSE.
 
 
+            if( T1_EntryMatriz .eqv. .TRUE.) then
+                DELTI=1.0/DELT
 
-            !           ***  INITIALIZE OUTPUT ARRAYS AND PARAMETERS  ***
+                !           ***  INITIALIZE OUTPUT ARRAYS AND PARAMETERS  ***
 
-            FT=0.0;FQ=0.0;FU=0.0;FV=0.0;FTRA=0.0 ! Equivale a do 5 de convect43c.f
+                FT=0.0;FQ=0.0;FU=0.0;FV=0.0;FTRA=0.0 ! Equivale a do 5 de convect43c.f
 
-            do_seven: DO  I=1,NL+1
-                          RDCP=(RD*(1.-Q(I))+Q(I)*RV)/(CPD*(1.-Q(I))+Q(I)*CPV)
-                          TH(I)=T(I)*(1000.0/P(I))**RDCP !Temperatura Potencial do ar umido
-                      enddo do_seven
+                do_seven: DO  I=1,NL+1
+                              RDCP=(RD*(1.-Q(I))+Q(I)*RV)/(CPD*(1.-Q(I))+Q(I)*CPV)
+                              TH(I)=T(I)*(1000.0/P(I))**RDCP !Temperatura Potencial do ar umido
+                          enddo do_seven
 
-            PRECIP=0.0; WD=0.0; TPRIME=0.0; QPRIME=0.0; IFLAG=0
+                PRECIP=0.0; WD=0.0; TPRIME=0.0; QPRIME=0.0; IFLAG=0
 
-            if(IPBL /= 0 ) then
-               call AdiabaticAdjustment(NL,NA,ND,NTRA,TH,Q,U,V,PH,TRA,TRATM,DELT)
-            endif
+                if(IPBL /= 0 ) then
+                   call copem_AdiabaticAdjustment()
+                endif
+!
+!
+!
+!
+!
+!
+            else
+                print*,'An erro in the input parameters'
+                continue
+            end if
 
-        end subroutine convection
+            call copem_free_matriz
+
+
+        end subroutine  copem_convection
 !
 !!
 !! PERFORM DRY ADIABATIC ADJUSTMENT
 !
-        subroutine AdiabaticAdjustment(NL,NA,ND,NTRA,TH,Q,U,V,PH,TRA,TRATM,DELT)
+        subroutine copem_AdiabaticAdjustment
+            implicit none
             logical :: lcomp
-            integer :: JC,JN,NL,NTRA,ND,NA
-            integer :: I,J,K
-            real(kind=8) :: soma,THBAR,AHM,RM,UM,VM,DPHINV,A2,x,RDCP,TNEW,ALV,ALVNEW,DELT, &
-                            PRECIP,QNEW,TC
-            real(kind=8), dimension(NTRA) :: TRANTM
-            real(kind=8), dimension(ND) ::  T,Q,QS,U,V,P,PH,FT,FQ,FU,FV
-            real(kind=8), dimension(ND,NTRA) :: TRA,FTRA
-            real(kind=8), dimension(NA) :: NENT,UP,VP,M,MP,TVP,TV,WATER,QP,EP,TH,WT,EVAP,CLW, &
-                                           SIGP,TP,TOLD,CPN,LV,LVCP,H,HP,GZ,HM,TRATM
+            integer :: JC,JN,ierr
+            real :: soma,THBAR,AHM,RM,UM,VM,DPHINV,A2,x,RDCP,TNEW,ALV,ALVNEW, &
+                            QNEW,TC
+            real, dimension(NA) :: TH,TRATM,TOLD
+
   
- 
 
             lcomp=.true.
 
@@ -357,7 +393,121 @@ module convect_emanuel
                 enddo do_thirty_eight  
             END IF
 
-        end subroutine AdiabaticAdjustment
-end module  convect_emanuel
+
+        end subroutine copem_AdiabaticAdjustment
+
+!!The Subroutine copem_set_matriz_at1 make a test of the dimension of the arrays  T1,Q1,QS1,P1,PH1,FT1,FQ1
+!! And allocate the equivalente arrays T,Q,QS,P,PH,FT,FQ
+!! These array must have same dimension ND. If its OK, all matriz are dimensioned.
+
+
+        subroutine copem_set_matriz_at1(T1,Q1,QS1,U1,V1,TRA1,P1,PH1)
+        integer :: N1,N2,N3,N4,N5,N6,N7,N8,N9
+        real, dimension(:) :: T1,Q1,QS1,U1,V1,P1,PH1
+        real, dimension(:,:) :: TRA1
+        integer, dimension(2) :: NDNTRA
+        integer :: ierr1,ierr2,ierr3,ierr4,ierr5,ierr6,ierr7,ierr8,ierr9,ierr10,ierr11,ierr12,ierr13
+
+
+            N1=size(T1); N2=size(Q1); N3=size(QS1); N4=size(U1); N5=size(V1); N6=size(P1); N7=size(PH1)
+            NDNTRA = shape(TRA1)
+            N8=NDNTRA(1)
+            N9=NDNTRA(2)
+
+            NTRA = N9
+
+            
+            if(N1 == N2 .and. N2 == N3 .and. N3 == N4 .and. N4 == N5 .and. N5 == N6 .and. N6 == N8) then
+                ND= N6
+                if(N7 == N6 +1 ) then
+                    NA=ND+1
+                    T1_EntryMatriz = .TRUE.
+                    allocate(T(ND),stat=ierr1)
+                    if(ierr1 /= 0) print*, 'Allocated error T'
+                    allocate(Q(ND),stat=ierr2)
+                    if(ierr2 /= 0) print*, 'Allocated error Q'
+                    allocate(QS(ND),stat=ierr3)
+                    if(ierr3 /= 0) print*, 'Allocated error QS'
+                    allocate(U(ND),stat=ierr4)
+                    if(ierr4 /= 0) print*, 'Allocated error U'
+                    allocate(V(ND),stat=ierr5)
+                    if(ierr5 /= 0) print*, 'Allocated error V'
+                    allocate(P(ND),stat=ierr6)
+                    if(ierr6 /= 0) print*, 'Allocated error P'
+                    allocate(PH(NA),stat=ierr7)
+                    if(ierr7 /= 0) print*, 'Allocated error U'
+                    allocate(TRA(ND,NTRA),stat=ierr8)
+                    if(ierr8 /= 0) print*, 'Allocated error TRA'
+                    allocate(TH(NA),stat=ierr9)
+                    if(ierr9 /= 0) print*, 'Allocated error TRA'
+
+
+                    allocate(FT(ND),stat=ierr10)
+                    if(ierr10 /= 0) print*, 'Allocated error TRA'                   
+                    allocate(FQ(ND),stat=ierr11)
+                    if(ierr11 /= 0) print*, 'Allocated error TRA'
+                    allocate(FU(ND),stat=ierr12)
+                    if(ierr12 /= 0) print*, 'Allocated error TRA'
+                    allocate(FV(ND),stat=ierr13)
+                    if(ierr13 /= 0) print*, 'Allocated error TRA'
+
+
+
+                    if(ierr1 /= 0 .or. ierr2 /= 0 .or. ierr3 /= 0 .or. ierr4 /= 0 &
+                       .or. ierr5 /= 0 .or. ierr6 /= 0 .or. ierr7 /= 0 .or. ierr8 /= 0 ) then
+
+                        print*,"Allocated error, verify the code"
+                        T1_EntryMatriz = .FALSE.
+                        call copem_free_matriz
+
+                    endif
+                   
+                else
+                    print*,"The Dimension of PH array must be equal",ND,&
+                           "+1, but it has dimension of ",N7
+                    print*,"The  copem_convection will do nothing until this correction be made."
+                    print*,"Verify your datas"
+                    print*,"Good look for you"
+                    T1_EntryMatriz = .FALSE.
+
+                endif
+
+
+
+            else
+                print*,"The Dimension of arrays T,Q,QS,U,V and P, and the numbers of lines of TRA, must be equals,",&
+                       "but they  have, respecively, the following dimensions: ",N1,N2,N3,N4,N5,N6,N8
+                print*,"The  copem_convection will do nothing until this correction be made."
+                print*,"Verify your datas."
+                print*,"Good look for you."
+                T1_EntryMatriz = .FALSE.
+
+            endif
+
+            
+
+        end subroutine copem_set_matriz_at1
+!!
+!! This subroutine is used to deallocate arrays T,Q,QS,P,PH,FT,FQ and matriz TRA
+!!
+        subroutine copem_free_matriz
+            deallocate(T)
+            deallocate(Q)
+            deallocate(QS)
+            deallocate(U)
+            deallocate(V)
+            deallocate(P)
+            deallocate(PH)
+            deallocate(TRA)
+            deallocate(TH)
+            deallocate(FV)
+            deallocate(FU)
+            deallocate(FQ)
+            deallocate(FT)
+            
+
+        end subroutine copem_free_matriz
+
+end module  copem
 
 
